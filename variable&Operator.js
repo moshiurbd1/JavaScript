859fc0e19e0388c0de83f07d93c47abba74776d2
moshiurbd1 <moshiurbd101@gmail.com>
2025-04-29 10:53:33 +0600

    JavaScript variable and operators

---
+++ variable&Operator.js
@@ -0,0 +1,43 @@
+/* variable contain data */
+//number
+var x=10;
+
+//string
+var subject="CSE"
+
+//boolean
+var passed=true;
+
+/* Naming convention 
+1. single word
+2. no quote
+3. no gap or dash
+4. can not start with number
+5. can not be a keyword
+6. prefer camel case
+7. case sensitive
+*/
+
+/* Operations
+1. +, -, *, /
+2. +=, -=, *=, /=
+3. isNan
+4. null
+5. Number
+*/
+
+/* Comparison 
+1. Greater than: >
+2. Less than: <
+3. Equal: ==
+4. Less than or equal: <=
+5. Greater than or equal: >=
+6. Not equal: !=
+
+Logicla operator:
+
+And: &&
+Or: ||
+Not: !
+
+*/
