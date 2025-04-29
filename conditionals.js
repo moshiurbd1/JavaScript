587f1015bc5a93ff216ad80365ee374f11cde5b3
moshiurbd1 <moshiurbd101@gmail.com>
2025-04-29 11:06:57 +0600

    Conditionals added

---
+++ conditionals.js
@@ -0,0 +1,23 @@
+/*Conditionals
+
+if(conditon){
+    Expression
+}
+
+if(conditon){
+    Expression
+}else{
+    Expression
+}
+
+*/
+var x=10;
+if(x>5){
+    console.log("Yes");
+}
+
+if(x>20){
+    console.log("Yes");
+}else{
+    console.log("Less than 20");
+}
