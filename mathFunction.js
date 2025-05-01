35f297ceeba0db292f729e3d04fa9d7ba9a22ca0
moshiurbd1 <moshiurbd101@gmail.com>
2025-05-01 22:58:22 +0600

    Math object's function min,max,PI,abs,floor,ceil,round and random

---
+++ mathFunction.js
@@ -0,0 +1,12 @@
+console.log(Math.min);
+const x=Math.min(121,33,4,22,5,1);
+console.log(x);
+const y=Math.max(121,33,4,22,5,1);
+console.log(y);
+console.log(Math.PI);
+console.log(Math.abs(-5));
+console.log(Math.floor(1.6));
+console.log(Math.ceil(1.6));
+console.log(Math.round(1.6));
+
+console.log(Math.round(Math.random()*10));
