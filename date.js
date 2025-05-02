746e042f7132d540225c0d3e58c905d85b1e7f6b
moshiurbd1 <moshiurbd101@gmail.com>
2025-05-03 00:16:22 +0600

    Date object, getDate, getMonth, getDay added

---
+++ date.js
@@ -0,0 +1,10 @@
+const today=new Date();
+console.log(today);
+const date= new Date("2062-10-19");
+console.log(date);
+console.log(date.getMonth());
+const specificDate=new Date(2091,0,26);
+console.log(specificDate);
+console.log(specificDate.toLocaleDateString("en-GB"));
+
+//unix epoc
