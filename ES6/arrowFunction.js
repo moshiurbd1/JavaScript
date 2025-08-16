b7c1cb190dd567785088938e77e5713a88224ff8
moshiurbd1 <moshiurbd101@gmail.com>
2025-08-16 11:45:20 +0600

    Spread Operator added

--- ES6/arrowFunction.js
+++ ES6/arrowFunction.js
@@ -8,3 +8,12 @@ console.log(square(4));
 //without parameter
 const pi=()=>Math.PI;
 console.log(pi());
+
+//large arrow function 
+
+const doMath=(a,b,c)=>{
+    const sum=a+b+c;
+    const mult=a*b*c;
+    return sum+mult;
+}
+console.log(doMath(5,10,15));
---
+++ ES6/spreadOperator.js
@@ -0,0 +1,14 @@
+const numbers=[1,2,3,4,5];
+const num2=numbers;
+num2.push(10);
+console.log(numbers);
+console.log(num2);// Both output will change due to contianing reference that is why we use spread operator
+const num3=[...numbers];
+num3.push(500);
+console.log(numbers);
+console.log(num3);
+// we can add more value before and after spread operator
+const num4=[7,8,...numbers,9];
+console.log(num4);
+
+console.log(Math.max(...num4));
