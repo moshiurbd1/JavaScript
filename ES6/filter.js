8e7ae1dd6fce852ca05a5e49e0591ad673c7a75f
moshiurbd1 <moshiurbd101@gmail.com>
2025-08-17 13:31:13 +0600

    filter function added

---
+++ ES6/filter.js
@@ -0,0 +1,9 @@
+const numbers=[1,2,3,4,5,6];
+const nums=numbers.filter(num=>num%2==0);
+console.log(nums);
+const num2=numbers.filter(n=>n>2);
+console.log(num2);
+const names=['am','jam','kathal','lichu'];
+const filterNames=names.filter(name=>name.length>2);
+console.log(filterNames);
+//Filter function return array with condition
