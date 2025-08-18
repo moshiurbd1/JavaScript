4d1ce8451aabc09add64d53caf85716a447950bf
moshiurbd1 <moshiurbd101@gmail.com>
2025-08-18 18:45:16 +0600

    call back function added

---
+++ ES6/callBackFunction.js
@@ -0,0 +1,11 @@
+function greetings(morningGreetings,name){
+    morningGreetings(name);
+}
+
+function morningGreetings(name){
+    console.log('Good morning!',name);
+}
+
+greetings(morningGreetings,'Torikul');
+//callback function call another function inside a function
+
