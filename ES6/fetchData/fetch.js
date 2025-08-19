a9e241d6c61138a86813026f1a01ab5d43a906a8
moshiurbd1 <moshiurbd101@gmail.com>
2025-08-20 01:47:34 +0600

    Fetch Data

---
+++ ES6/fetchData/fetch.js
@@ -0,0 +1,34 @@
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())//.json() is not similar but close to JSON.parse()
+      //and conver data to javaScript object
+      .then(json => console.log(json))
+
+
+      function bringData(){
+        const api="https://jsonplaceholder.typicode.com/todos/1";
+        fetch(api)
+        .then(response=>response.jso())//json()
+        .then(json=>console.log(json))
+        .catch(error=>console.log('Error happend',error))
+
+      }
+
+      function userDataLoad(){
+        const api='https://jsonplaceholder.typicode.com/users';
+        fetch(api)
+        .then(res=>res.json())
+        // .then(users=>console.log(users));
+        .then(users=>dispayUser(users))
+      }
+
+      function dispayUser(users){
+        console.log(users);
+        users.forEach(element => {
+            const ul=document.createElement('ul');
+            document.body.appendChild(ul);
+            const li=document.createElement('li');
+            li.innerText=element.name;
+            ul.appendChild(li);
+        });
+      }
+
